a700f19680b3043284a0fcd617ec65c028518a4f
itorres <itorres@peertransfer.com>
2015-04-28 18:32:37 +0200

    only detects winner for higher card

--- croupier.rb
+++ croupier.rb
@@ -1,4 +1,51 @@
 class Croupier
+  RANK = "Highest card"
+  SEPARATOR = ": "
+  FACE_VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+  WINSWITH = " wins with " + RANK + SEPARATOR
 
-  
+  def self.check hand_one, hand_two
+    player_one_sorted_hand = sort_hand(hand_one)
+    player_two_sorted_hand = sort_hand(hand_two)
+    winner = ""
+
+    (1..5).each do |position|
+      player_one_next_highest_card = player_one_sorted_hand[position-1] 
+      player_two_next_highest_card = player_two_sorted_hand[position-1] 
+      
+      next if worth(player_one_next_highest_card) == worth(player_two_next_highest_card)
+      winner = "Player one" + WINSWITH + player_one_next_highest_card
+      winner = "Player two" + WINSWITH + player_two_next_highest_card if (worth(player_one_next_highest_card) < worth(player_two_next_highest_card))
+      break
+    end
+    winner
+  end
+
+  def self.check_hand a_hand
+    RANK + SEPARATOR + highest_card(a_hand)
+  end
+
+  def self.highest_card a_hand
+    sorted_cards = sort_hand a_hand
+    sorted_cards.first 
+  end
+
+  def self.sort_hand a_hand
+    cards = a_hand.split(" ")
+    sorted_cards = []
+    FACE_VALUES.reverse.each do |value|
+      cards.each do |card|
+        sorted_cards << card if value(card) == value
+      end
+    end
+    sorted_cards
+  end
+
+  def self.worth card
+    FACE_VALUES.index(value(card))
+  end
+
+  def self.value card
+    card[0,1]
+  end
 end
--- spec/croupier_spec.rb
+++ spec/croupier_spec.rb
@@ -1,5 +1,29 @@
 require './croupier'
 
 describe "A croupier in the casino " do
-	
+  context 'when checking a player hand' do
+    it "identifies the highest card" do
+      expect(Croupier.check_hand("2H 3D 5S 9C KD")).to eq("Highest card: KD")
+      expect(Croupier.check_hand("2H 3D AS 9C KD")).to eq("Highest card: AS")  
+      expect(Croupier.check_hand("2H 3D 4H 9C 6D")).to eq("Highest card: 9C") 
+    end
+  end
+
+  context 'when checking two players hands' do
+    it "identifies the winner with the highest card" do
+      player_one_hand = "2H 3D 5S 9C KD"
+      player_two_hand = "2H 3D 4H 9C 6D"
+      expected = "Player one wins with Highest card: KD"
+      reverse_expected = "Player two wins with Highest card: KD"
+      expect(Croupier.check(player_one_hand,player_two_hand)).to eq(expected)
+      expect(Croupier.check(player_two_hand,player_one_hand)).to eq(reverse_expected)
+    end
+
+    it "idenifies the winner when the highest card is the same" do
+      expected = "Player one wins with Highest card: 4D"
+      player_one_hand = "3H 4D 5S 9C KD"
+      player_two_hand = "2H 3D 5S 9C KD"
+      expect(Croupier.check(player_one_hand,player_two_hand)).to eq(expected)
+    end
+  end
 end
